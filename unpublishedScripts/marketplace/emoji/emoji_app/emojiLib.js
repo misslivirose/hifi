0e5d7cc00cda38b509da6219477460cdadfee076
Livi Erickson <livi@outlook.com>
2017-07-19 09:29:51 -0700

    change to use relative paths for json files instead of S3

--- unpublishedScripts/marketplace/emoji/emoji_app/emojiLib.js
+++ unpublishedScripts/marketplace/emoji/emoji_app/emojiLib.js
@@ -1,18 +1,15 @@
 // emoji library elisa
-
-// links JSON file from AWS to var
 module.exports = {
 
 	emojiLib: {
-		corn: "https://hifi-content.s3.amazonaws.com/elisalj/emoji_scripts/JSON_files/corn.json?" + Date.now(),
-		fireworks: "https://hifi-content.s3.amazonaws.com/elisalj/emoji_scripts/JSON_files/fireworks.json?" + Date.now(),
-		flower: "https://hifi-content.s3.amazonaws.com/elisalj/emoji_scripts/JSON_files/flower.json?" + Date.now(),
-		heart: "https://hifi-content.s3.amazonaws.com/elisalj/emoji_scripts/JSON_files/heart.json?" + Date.now(),
+		corn: "./JSON_files/corn.json?" + Date.now(),
+		fireworks: "./JSON_files/fireworks.json?" + Date.now(),
+		flower: "./JSON_files/flower.json?" + Date.now(),
+		heart: "./JSON_files/heart.json?" + Date.now(),
 		monster: "./JSON_files/monster.json",
-		//monster: "https://hifi-content.s3.amazonaws.com/elisalj/emoji_scripts/JSON_files/monster.json?" + Date.now(),
-		pickle: "https://hifi-content.s3.amazonaws.com/elisalj/emoji_scripts/JSON_files/pickle.json?" + Date.now(),
-		pizza: "https://hifi-content.s3.amazonaws.com/elisalj/emoji_scripts/JSON_files/pizza.json?" + Date.now(),
-		poo: "https://hifi-content.s3.amazonaws.com/elisalj/emoji_scripts/JSON_files/poo.json?" + Date.now()
+		pickle: "./JSON_files/pickle.json?" + Date.now(),
+		pizza: "./JSON_files/pizza.json?" + Date.now(),
+		poo: "./JSON_files/poo.json?" + Date.now()
 	},
 
 	getEmoji: function(name, library) {
